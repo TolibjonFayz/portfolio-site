a97535c2fda7206d8eb3ac09f007ed11ba05ca33
Tolibjon04 <tolibsaydulloyevich1@gmail.com>
2023-12-12 12:08:50 +0500

    bugs fixed

--- src/components/Hours.vue
+++ src/components/Hours.vue
@@ -70,11 +70,11 @@
     align-items: center;
   }
   #second {
-    margin-right: 32px;
+    margin-right: 30px;
     margin-top: -4px;
   }
   #third {
-    margin-right: 32px;
+    margin-right: 31px;
     margin-top: -4px;
     margin-bottom: -4px;
   }
